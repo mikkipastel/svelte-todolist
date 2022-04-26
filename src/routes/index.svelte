2f31143eb5ac6d8eb61b7fc159bb786aea6cf3fa
Monthira Chayabanjonglerd <monthira@ookbee.com>
2022-04-26 20:03:01 +0700

    add header text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	export let name = "world";
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+</main>
